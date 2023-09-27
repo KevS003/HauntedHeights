d8539b2752a8e2b0abac98d018b7fdb369ede485
bvo29 <112092743+bvo29@users.noreply.github.com>
2023-09-27 17:09:43 -0400

    Basic movement

---
+++ Haunted Heights/Assets/Scripts/CubeController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+gameManager GameManager;
+ 
+    void Start()
+    {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+        GameManager = gameController.GetComponent<gameManager>();
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(GameManager.moveVector * GameManager.moveSpeed * Time.deltaTime);
+    }
+}
