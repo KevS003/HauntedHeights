a788b4d61a925862bcbf424f2300323b9980c3df
bvo29 <bvocubs@gmail.com>
2023-10-11 17:17:36 -0400

    Distance Shit

---
+++ Haunted Heights/Assets/Scripts/EnemyTracker.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyTracker : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject player;
+    public GameObject enemyTracker;
+    private TextMeshProUGUI enemyTrackerText;
+
+    private float Distance;
+
+    void Start()
+    { 
+        enemyTrackerText = enemyTracker.GetComponent<TextMeshProUGUI>();
+    }
+    
+    void FixedUpdate(){
+    float y2 = player.transform.position.y;
+    float y1 = enemy.transform.position.y;
+
+    float x2 = player.transform.position.x;
+    float x1 = player.transform.position.x;
+
+    float newY = y2 - y1;
+    float newX = x2 - x1;
+
+    
+    float Y = newY * newY;
+    float X = newX * newX;
+
+    float Answer = Y + X;
+
+    Distance = Mathf.Pow (Answer,Answer);
+
+    Debug.Log(Distance);
+
+    }
+
+    void Update()
+    {
+        enemyTrackerText.text = Distance.ToString();
+    }
+
+}
