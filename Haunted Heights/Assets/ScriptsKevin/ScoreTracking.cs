66ec022bf0125043e132265f151d262d2b1e34c9
KevS003 <kjscordero@gmail.com>
2023-10-11 17:14:38 -0400

    Powerup 1 done
    Score mutl works

--- Haunted Heights/Assets/ScriptsKevin/ScoreTracking.cs
+++ Haunted Heights/Assets/ScriptsKevin/ScoreTracking.cs
@@ -7,6 +7,10 @@ public class ScoreTracking : MonoBehaviour //controls UI and score tracking. Cou
 {
     public GameObject scoreCount;
     private TextMeshProUGUI scoreCountText;
+    private bool playerDouble= false;
+    [SerializeField]
+    private float powerUpTime = 10f;
+    private float powerTimeOG;
 
 
     public int totalScore=0;
@@ -14,16 +18,40 @@ public class ScoreTracking : MonoBehaviour //controls UI and score tracking. Cou
     void Start()
     {
         scoreCountText = scoreCount.GetComponent<TextMeshProUGUI>();
+        powerTimeOG = powerUpTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreCountText.text = totalScore.ToString();
+
+        if(playerDouble == true && powerUpTime > 0.0f)
+        {
+            powerUpTime-= Time.deltaTime;
+            Debug.Log(powerUpTime);
+        }
+        else if(powerUpTime <= 0.0f)
+        {
+            playerDouble = false;
+            powerUpTime = powerTimeOG;
+        }
     }
 
     public void PlayerScored()
     {
-        totalScore++;
+        if(playerDouble == false)
+            totalScore++;
+        else if(playerDouble == true)
+        {
+            Debug.Log("We score two");
+            totalScore+=2;
+        }
+    }
+    public void PlayerDouble()
+    {
+        Debug.Log("I here");
+        if(playerDouble==false)
+            playerDouble = true;
     }
 }
