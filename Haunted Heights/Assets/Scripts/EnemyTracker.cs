f70896e59eee96466b97581262ae4a19d0ca3aca
bvo29 <bvocubs@gmail.com>
2023-10-13 01:25:29 -0400

    EnemyTracker

--- Haunted Heights/Assets/Scripts/EnemyTracker.cs
+++ Haunted Heights/Assets/Scripts/EnemyTracker.cs
@@ -10,15 +10,18 @@ public class EnemyTracker : MonoBehaviour
     public GameObject enemyTracker;
     private TextMeshProUGUI enemyTrackerText;
 
-    private float Distance;
+    
+
+    private float distance;
 
     void Start()
     { 
         enemyTrackerText = enemyTracker.GetComponent<TextMeshProUGUI>();
     }
     
-    void FixedUpdate(){
-    float y2 = player.transform.position.y;
+    void FixedUpdate()
+    {
+    /*float y2 = player.transform.position.y;
     float y1 = enemy.transform.position.y;
 
     float x2 = player.transform.position.x;
@@ -35,13 +38,14 @@ public class EnemyTracker : MonoBehaviour
 
     Distance = Mathf.Pow (Answer,Answer);
 
-    Debug.Log(Distance);
+    Debug.Log(Distance); */
 
+       distance = Vector3.Distance (player.transform.position, enemy.transform.position);
     }
 
     void Update()
     {
-        enemyTrackerText.text = Distance.ToString();
+        enemyTrackerText.text = distance.ToString();
     }
 
 }
