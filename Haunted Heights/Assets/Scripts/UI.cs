d7eea97523f99947ebaeec1f2fcd2a36959092d6
KevS003 <kjscordero@gmail.com>
2023-10-22 18:37:38 -0400

    Adjusted scenes for build

--- Haunted Heights/Assets/Scripts/UI.cs
+++ Haunted Heights/Assets/Scripts/UI.cs
@@ -16,7 +16,7 @@ public class UI : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(0);
         }
 
     }
