76e00237a12e4f335a31acde9f39d7f425629734
KevS003 <kjscordero@gmail.com>
2023-10-25 17:13:20 -0400

    Leader board

--- Haunted Heights/Assets/ScriptsKevin/LeaderBoard.cs
+++ Haunted Heights/Assets/ScriptsKevin/LeaderBoard.cs
@@ -7,23 +7,13 @@ using UnityEngine;
 public class LeaderBoard : MonoBehaviour
 {
     public List<int> leaderBoard;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //update leaderboard with a new score until 10 then start overwritting lowest scores
-    }
 
     public void leadUpdate(int score)
     {
         //topScores = topScores.
         leaderBoard.Add(score);
         IEnumerable<int> leaderboard = leaderBoard.OrderByDescending(x => x).Take(10).ToList();
+        Debug.Log("LeaderBoard: " + leaderBoard[0]);
 
 
     }
