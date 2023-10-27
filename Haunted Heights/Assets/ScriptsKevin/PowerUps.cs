59d2bb397bde48b436f27948ed7290fdef61192a
KevS003 <kjscordero@gmail.com>
2023-10-27 17:29:27 -0400

    Revert "Unsafe push"
    This reverts commit c8620107b5d975091644b5447480ee9f6711cea1.

--- Haunted Heights/Assets/ScriptsKevin/PowerUps.cs
+++ Haunted Heights/Assets/ScriptsKevin/PowerUps.cs
@@ -83,6 +83,7 @@ public class PowerUps : MonoBehaviour
             //autobuildtiles
             //UI feedback in destroy click
             autoDestroyNails.AutoBuild(autoDestructTime);
+            Destroy(gameObject);
         }
         else if(speedUpPlayer)
         {
